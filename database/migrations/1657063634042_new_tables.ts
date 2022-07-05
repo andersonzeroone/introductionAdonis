0f1bc89a559f8a9fdf1070c868733bd523ecd57e
Anderson Pablo <anderson.pablo02@gmail.com>
2022-07-05 20:52:10 -0300

    test migrations

---
+++ database/migrations/1657063634042_new_tables.ts
@@ -0,0 +1,13 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {})
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {})
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,10 @@
 |
 */
 
+import Database from '@ioc:Adonis/Lucid/Database'
+
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hello: 'world' }
+  return Database.from('users').select('*')
 })
