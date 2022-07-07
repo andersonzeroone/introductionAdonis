124bd902bdced61a997da7eed14466f4831ace39
Anderson Pablo <anderson.pablo02@gmail.com>
2022-07-06 21:31:40 -0300

    creating the first controller

--- start/routes.ts
+++ start/routes.ts
@@ -26,14 +26,4 @@ Route.get('/', async () => {
   return Database.from('users').select('*')
 })
 
-Route.post('login', async ({ auth, request, response }) => {
-  const email = request.input('email')
-  const password = request.input('password')
-
-  try {
-    const token = await auth.use('api').attempt(email, password)
-    return token
-  } catch {
-    return response.unauthorized('Invalid credentials')
-  }
-})
+Route.post('login', 'AuthController.login')
