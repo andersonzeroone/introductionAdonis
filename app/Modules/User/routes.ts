43c38325743d1ed8155bb28deaaee787a2db6e60
Anderson Pablo <anderson.pablo02@gmail.com>
2022-07-07 01:17:51 -0300

    add controller in routes group

--- app/Modules/User/routes.ts
+++ app/Modules/User/routes.ts
@@ -18,7 +18,10 @@ Route.get('docs/*', ({ params }) => {
 })
 
 Route.group(() => {
-  Route.get('/users', async () => {
-    return User.all()
-  })
+  Route.get('/users', 'UsersController.index')
+
+  Route.post('users', 'UsersController.store')
+  Route.get('users/:id', 'UsersController.show')
+  Route.put('users/:id', 'UsersController.update')
+  Route.delete('users/:id', 'UsersController.destroy')
 }).middleware('auth')
