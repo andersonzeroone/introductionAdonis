b3e55ef5fff125d448c39b2ff838218213e21a56
Anderson Pablo <anderson.pablo02@gmail.com>
2022-07-07 01:23:06 -0300

    add controller resource

--- app/Modules/User/routes.ts
+++ app/Modules/User/routes.ts
@@ -17,11 +17,15 @@ Route.get('docs/*', ({ params }) => {
   console.log(params['*'])
 })
 
-Route.group(() => {
-  Route.get('/users', 'UsersController.index')
+// Route.group(() => {
+//   Route.get('/users', 'UsersController.index')
 
-  Route.post('users', 'UsersController.store')
-  Route.get('users/:id', 'UsersController.show')
-  Route.put('users/:id', 'UsersController.update')
-  Route.delete('users/:id', 'UsersController.destroy')
+//   Route.post('users', 'UsersController.store')
+//   Route.get('users/:id', 'UsersController.show')
+//   Route.put('users/:id', 'UsersController.update')
+//   Route.delete('users/:id', 'UsersController.destroy')
+// }).middleware('auth')
+
+Route.group(() => {
+  Route.resource('/users', 'UsersController')
 }).middleware('auth')
